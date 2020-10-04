64821165bf7b52f228c60bd039f5270e8606feba
unnati1201 <64747335+unnati1201@users.noreply.github.com>
2020-10-05 01:09:26 +0530

    Add files via upload

---
+++ DiceGame/index.js
@@ -0,0 +1,15 @@
+var randomNumber1 = Math.random();
+randomNumber1 = Math.floor(randomNumber1*6) + 1;
+document.querySelectorAll("img")[0].setAttribute("src","images/dice"+randomNumber1+".png");
+
+randomNumber2 = Math.random();
+randomNumber2 = Math.floor(randomNumber2*6) + 1;
+document.querySelectorAll("img")[1].setAttribute("src","images/dice"+randomNumber2+".png");
+
+if(randomNumber1>randomNumber2){
+  document.querySelector("h1").innerHTML="🚩Player 1 Wins!";
+}else if(randomNumber1<randomNumber2){
+  document.querySelector("h1").innerHTML="Player 2 Wins!🚩";
+}else{
+  document.querySelector("h1").innerHTML="Draw";
+}
